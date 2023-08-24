93f6bf4bcf304d6b3af778051fbfa3fcc3091ab7
SamikshaGhongade <samikshaaghongade@gmail.com>
2023-08-24 11:18:22 +0530

    commit1

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+//Fetch div of id=counter
+const countValue = document.querySelector('#counter');
+
+
+const increment = () => {
+    //get the value from UI
+    let value = parseInt(countValue.innerText);
+    //Update the value
+    value = value + 1;
+    //set the value onto UI
+    countValue.innerText = value;
+}
+
+const decrement = () => {
+
+     //get the value from UI
+     let value = parseInt(countValue.innerText);
+     //Update the value
+     value = value - 1;
+     //set the value onto UI
+     countValue.innerText = value;
+}
+
+
+// Using EventListener
+
